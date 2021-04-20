036e01e756adfe8aceb2c4acf209d60f5fc99928
RianMupti <rianmupti@gmail.com>
2021-04-20 09:34:48 +0700

    memahami siklus hidup functional component

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const App = () => {
         {/* <SampleComponent /> */}
         {/* <StylingComponent /> */}
         {show && <FlexBox />}
+        {/* <FlexBox /> */}
         {/* <Position /> */}
       </ScrollView>
     </View>
--- src/pages/FlexBox/index.js
+++ src/pages/FlexBox/index.js
@@ -1,66 +1,118 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { Text, View, Image } from 'react-native';
 import lily from '../../assets/images/lily.jpg';
 
-class FlexBox extends Component {
-    constructor(props) {
-        super(props);
-        console.log("==> constructor")
-        this.state = {
-            subscriber: 100
-        }
-    }
+// class FlexBox extends Component {
+//     constructor(props) {
+//         super(props);
+//         console.log("==> constructor")
+//         this.state = {
+//             subscriber: 100
+//         }
+//     }
 
-    componentDidMount() {
-        console.log("==> component did mount")
+//     componentDidMount() {
+//         console.log("==> component did mount")
+//         setTimeout(() => {
+//             this.setState({
+//                 subscriber: 400
+//             });
+//         }, 3000);
+//     }
+
+//     componentDidUpdate() {
+//         console.log("==> component did update")
+//     }
+
+//     componentWillUnmount() {
+//         console.log("==> component will unmount")
+//     }
+
+//     render() {
+//         console.log("==> render")
+//         return (
+//             <View>
+//                 <View style={{
+//                     flexDirection: 'row',
+//                     backgroundColor: 'grey',
+//                     alignItems: 'center',
+//                     justifyContent: 'space-between'
+//                 }}>
+//                     <View style={{ backgroundColor: 'red', width: 50, height: 50 }} />
+//                     <View style={{ backgroundColor: 'yellow', width: 50, height: 50 }} />
+//                     <View style={{ backgroundColor: 'blue', width: 50, height: 50 }} />
+//                     <View style={{ backgroundColor: 'purple', width: 50, height: 50 }} />
+//                 </View>
+//                 <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+//                     <Text>Beranda</Text>
+//                     <Text>Video</Text>
+//                     <Text>Playlist</Text>
+//                     <Text>Komunitas</Text>
+//                     <Text>Channel</Text>
+//                     <Text>Tentang</Text>
+//                 </View>
+//                 <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+//                     <Image source={lily} style={{ width: 100, height: 100, borderRadius: 50, margin: 20 }} />
+//                     <View >
+//                         <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Rian Mupti Jauhari</Text>
+//                         <Text>{this.state.subscriber} = Sang Pemalas</Text>
+//                     </View>
+//                 </View>
+//             </View>
+//         )
+//     }
+// }
+
+const FlexBox = () => {
+    const [subscriber, setSubscriber] = useState(300);
+
+    useEffect(() => {
+        console.log('did mount')
         setTimeout(() => {
-            this.setState({
-                subscriber: 400
-            });
+            setSubscriber(500)
         }, 3000);
-    }
+        return () => {
+            console.log('did update')
+        }
+    }, [subscriber])
 
-    componentDidUpdate() {
-        console.log("==> component did update")
-    }
+    // useEffect(() => {
+    //     console.log('did update')
+    //     setTimeout(() => {
+    //         setSubscriber(500)
+    //     }, 3000);
+    // }, [subscriber])
 
-    componentWillUnmount() {
-        console.log("==> component will unmount")
-    }
-
-    render() {
-        console.log("==> render")
-        return (
-            <View>
-                <View style={{
-                    flexDirection: 'row',
-                    backgroundColor: 'grey',
-                    alignItems: 'center',
-                    justifyContent: 'space-between'
-                }}>
-                    <View style={{ backgroundColor: 'red', width: 50, height: 50 }} />
-                    <View style={{ backgroundColor: 'yellow', width: 50, height: 50 }} />
-                    <View style={{ backgroundColor: 'blue', width: 50, height: 50 }} />
-                    <View style={{ backgroundColor: 'purple', width: 50, height: 50 }} />
-                </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-                    <Text>Beranda</Text>
-                    <Text>Video</Text>
-                    <Text>Playlist</Text>
-                    <Text>Komunitas</Text>
-                    <Text>Channel</Text>
-                    <Text>Tentang</Text>
-                </View>
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <Image source={lily} style={{ width: 100, height: 100, borderRadius: 50, margin: 20 }} />
-                    <View >
-                        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Rian Mupti Jauhari</Text>
-                        <Text>{this.state.subscriber} = Sang Pemalas</Text>
-                    </View>
+    return (
+        <View>
+            <View style={{
+                flexDirection: 'row',
+                backgroundColor: 'grey',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+            }}>
+                <View style={{ backgroundColor: 'red', width: 50, height: 50 }} />
+                <View style={{ backgroundColor: 'yellow', width: 50, height: 50 }} />
+                <View style={{ backgroundColor: 'blue', width: 50, height: 50 }} />
+                <View style={{ backgroundColor: 'purple', width: 50, height: 50 }} />
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                <Text>Beranda</Text>
+                <Text>Video</Text>
+                <Text>Playlist</Text>
+                <Text>Komunitas</Text>
+                <Text>Channel</Text>
+                <Text>Tentang</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Image source={lily} style={{ width: 100, height: 100, borderRadius: 50, margin: 20 }} />
+                <View >
+                    <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Rian Mupti Jauhari</Text>
+                    <Text>{subscriber} Sang Pemalas</Text>
                 </View>
             </View>
-        )
-    }
+        </View>
+    )
 }
 
 export default FlexBox;
