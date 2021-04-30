3b8d75f2d1b686d7d5d65e897ef040300782586c
RianMupti <rianmupti@gmail.com>
2021-04-30 19:27:55 +0700

    membuat component dinamis menggunakan props

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { View, ScrollView } from 'react-native';
 import FlexBox from './pages/FlexBox';
 import Position from './pages/Position';
+import PropsDinamis from './pages/PropsDinamis';
 import SampleComponent from './pages/SampleComponent';
 import StylingComponent from './pages/StylingComponent';
 
 const App = () => {
-  const [show, SetIsShow] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      SetIsShow(false)
-    }, 6000);
-  }, [])
+  // const [show, SetIsShow] = useState(true);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     SetIsShow(false)
+  //   }, 6000);
+  // }, [])
   return (
     <View>
       <ScrollView>
         {/* <SampleComponent /> */}
         {/* <StylingComponent /> */}
-        {show && <FlexBox />}
+        {/* {show && <FlexBox />} */}
         {/* <FlexBox /> */}
         {/* <Position /> */}
+        <PropsDinamis />
       </ScrollView>
     </View>
   )
